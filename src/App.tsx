c1b3acf367f3fd4cfdc0fa0800ec3fed5c06d1d9
Pablo Navarro Ortigosa <pablonav.dev@gmail.com>
2026-03-24 11:34:38 +0100

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
+import HomePage from './pages/HomePage'
+
 
 function App() {
 
   return (
     <>
-    
+      <HomePage></HomePage>
     </>
   )
 }
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+import styles from "./Home.module.css"
+
+export default function HomePage() {
+
+    return (
+        <>
+            <p className={styles.p}>Hello World</p>
+        </>
+    )
+}
